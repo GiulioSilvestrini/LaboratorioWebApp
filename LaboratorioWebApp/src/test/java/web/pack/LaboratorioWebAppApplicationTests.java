c663370aad8ec7f748db85ba215e50ab6cf4a4c7
GiulioSilvestrini <138437976+GiulioSilvestrini@users.noreply.github.com>
2023-07-04 15:21:27 +0200

    Cambio (speriamo bene)

---
+++ LaboratorioWebApp/src/test/java/web/pack/LaboratorioWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package web.pack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
